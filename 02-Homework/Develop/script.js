84e64bdbe6d03cd69abb02847f00fd6cd80f08ab
Kristina <kashqar1117@gmail.com>
2020-11-20 20:11:21 -0600

    fist filter

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -25,7 +25,12 @@ function getLength(){
    specialCharacter = confirm("Do you want special characters?")
 
    if (!upperCase){
-     PossibleChoices = PossibleChoices.filter('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
+     PossibleChoices  = PossibleChoices.filter(function(character){
+       return character ='1', '2', '3', '4,', '5', '6', '7', '8', '9';
+
+     })
+
+    
    }
 
 
