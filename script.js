2fbe037f9ca45148f16d02a4148928bddc6a56ee
Kristina <kashqar1117@gmail.com>
2020-11-21 16:35:23 -0600

    cleaning up code

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 // Assignment Code
-
-let choice="";
 // var password;
-var length;
-var lengthChoice="";
+
 var lowerChoices =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var upperChoices =['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var specialChoices  = ["!","@", "#" , "$", "%", "^","&", "*"]
 var numberChoices= ['1', '2', '3', '4', '5', '6', '7', '8', '9' ]
 var PossibleChoices =[]
-var acceptpassword;
-
-  var lengthChoice = parseInt(prompt("How many characters do you want?(Choose between 8 & 128)"))
 
 
-  function getLength(){
+lengthChoice = parseInt(prompt("How many characters do you want?(Choose between 8 & 128)"))
+
+
+function getLength(){
+
+  
   
 
   if
@@ -23,7 +22,7 @@ var acceptpassword;
    {
      alert("invalid ammount, please choose between 8 & 128")
      getLength()
-     return;
+     
    }
   else 
    upperCase = confirm("Do you want uppercase letters?")
